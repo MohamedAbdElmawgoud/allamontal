19626ce8633e51986ba87303ad235b2328e5c4f1
Mohamed Abd ELmawgoud <49039674+MohamedAbdElmawgoud@users.noreply.github.com>
2020-02-18 13:43:56 +0200

    testDesign

--- src/app/view-customer/view-customer.page.ts
+++ src/app/view-customer/view-customer.page.ts
@@ -12,6 +12,13 @@ export class ViewCustomerPage implements OnInit {
   notes: any = [];
   note: any;
   Temp: any = [];
+  eldalfa:any;
+  elezaz:any;
+  wazn_el7elkan:any;
+  wazn_eldalfa:any;
+  wazn_elsekena:any;
+  else3r:any;
+  
   constructor(private router: Router, private route: ActivatedRoute, public storage: StorageService, public configuration: configuration) { }
 
   ngOnInit() {
@@ -26,18 +33,18 @@ export class ViewCustomerPage implements OnInit {
     for (count of temp) {
       this.Temp.push(this.notes[count]);
     }
-let test = this.configuration.makas_eldalfa(this.Temp[1] , this.Temp[2]);
-let test2 = this.configuration.makas_elezaz(this.Temp[1] , this.Temp[2]);
-let test3 = this.configuration.wazn_el7elkan(this.Temp[1] , this.Temp[2]);
-let test4 = this.configuration.wazn_eldalfa(this.Temp[1] , this.Temp[2]);
-let test5 = this.configuration.wazn_elsekena(this.Temp[1] , this.Temp[2]);
-let test6 = this.configuration.else3r(this.Temp[1] , this.Temp[2],this.Temp[3]);
-console.log('مقاس الضلف' , test);
-console.log('مقاس الازاز' ,test2)
-console.log('وزن الحلقان ', test3);
-console.log('وزن الضلفه ', test4);
-console.log('وزن السكينه ', test5);
-console.log('السعر ', test6);
+this.eldalfa = this.configuration.makas_eldalfa(this.Temp[1] , this.Temp[2]);
+this.elezaz = this.configuration.makas_elezaz(this.Temp[1] , this.Temp[2]);
+this.wazn_el7elkan = this.configuration.wazn_el7elkan(this.Temp[1] , this.Temp[2]);
+this.wazn_eldalfa = this.configuration.wazn_eldalfa(this.Temp[1] , this.Temp[2]);
+this.wazn_elsekena = this.configuration.wazn_elsekena(this.Temp[1] , this.Temp[2]);
+this.else3r = this.configuration.else3r(this.Temp[1] , this.Temp[2],this.Temp[3]);
+// console.log('مقاس الضلف' , test);
+// console.log('مقاس الازاز' ,test2)
+// console.log('وزن الحلقان ', test3);
+// console.log('وزن الضلفه ', test4);
+// console.log('وزن السكينه ', test5);
+// console.log('السعر ', test6);
 
 }
 
