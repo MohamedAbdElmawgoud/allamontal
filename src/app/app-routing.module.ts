341a884d55e2532eed80653d576bf6bd0ada2b2e
nader medhet <nadermedhet142@gmail.com>
2020-02-21 11:04:17 +0200

    add IsLoggedGuard guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { IsLoggedGuard } from './guards/is-logged.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', canActivate: [IsLoggedGuard], loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
   {
-    path: 'view-customer',  data: {note : ''},
-    loadChildren: () => import('./view-customer/view-customer.module').then( m => m.ViewCustomerPageModule)
+    path: 'view-customer', canActivate: [IsLoggedGuard], data: { note: '' },
+    loadChildren: () => import('./view-customer/view-customer.module').then(m => m.ViewCustomerPageModule)
   },
   {
     path: 'view-information',
-    loadChildren: () => import('./view-information/view-information.module').then( m => m.ViewInformationPageModule)
+    canActivate: [IsLoggedGuard],
+    loadChildren: () => import('./view-information/view-information.module').then(m => m.ViewInformationPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
 ];
 
